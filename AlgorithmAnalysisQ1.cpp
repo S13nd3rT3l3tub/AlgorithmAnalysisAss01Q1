ae47bf94df2d1dec88131e7dcbab0f2d0bd8bdae
louismineo <50332624+louismineo@users.noreply.github.com>
2023-09-19 16:14:57 +0800

    data read

--- AlgorithmAnalysisQ1.cpp
+++ AlgorithmAnalysisQ1.cpp
@@ -1,21 +1,46 @@
 // AlgorithmAnalysisQ1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include "input.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+
+
+	std::vector<std::vector<int>> hospital;
+	std::vector<std::vector<int>> residences;
+
+	hospital = read_hospital_file("data1_hospitals.txt");
+	//hospital = read_hospital_file(argv[1]);
+
+	residences = read_hospital_file("data1_residents.txt");
+
+	//residences = read_hospital_file(argv[2]);
+
+	printValues(hospital);
+	std::cout << std::endl;
+	printValues(residences);
+
+
+	/*
 	if (argc < 4) {
 		std::cout << "Usage: " << argv[0] << " <datafile_hospital> <datafile_residents> <hospital capacity(int)>\n";
 	}
 	else if (argc == 4) {
+		
 		<Problem data type> _problem{ argv[1], argv[2], argv[3] };
 		<Algorithm data type> _algorithm{ problem };
 		_algorithm.match();
+		
+
+		
+
+
 	}
 	else {
 		std::cout << "Too many arguments given!\n";
 		std::cout << "Usage: " << argv[0] << " <datafile_hospital> <datafile_residents> <hospital capacity(int)>\n";
 	}
+	*/
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
